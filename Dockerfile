95b49b8a6c8f40e342108202c2ea4ff591754e20
nakashimn <nakashimn@gmail.com>
2024-10-06 02:34:28 +0900

    modify:logging

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM public.ecr.aws/lambda/nodejs:20 as prod
 
+COPY ./package*.json /
 COPY ./src/app.js ${LAMBDA_TASK_ROOT}
+
+RUN npm ci
 
 CMD [ "app.handler" ]
--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
+const axios = require('axios');
+
 exports.handler = async (event) => {
+    const codebuild_project_name = event.detail['project-name'];
+    const build_number = event.detail['build-number'];
+    const build_id = event.detail['build-id'];
+    console.log(`[handler] DEBUG | [${codebuild_project_name}:${build_number}] build_id: ${build_id}`);
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
