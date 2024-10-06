38f264e1f362ff48591fdebb27aab981a2c9d694
nakashimn <nakashimn@gmail.com>
2024-10-06 14:27:57 +0900

    modify:message

--- src/app.js
+++ src/app.js
@@ -1,13 +1,42 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-    const codebuild_project_name = event.detail['project-name'];
-    const build_number = event.detail['build-number'];
-    const build_id = event.detail['build-id'];
-    console.log(`[handler] DEBUG | [${codebuild_project_name}:${build_number}] build_id: ${build_id}`);
+    // const
+    const appName = 'CodebuildNotification';
+    const account = event.account;
+    const region = event.region;
+    const buildDetails = event.detail;
+    const projectName = buildDetails['project-name'];
+    const buildStatus = buildDetails['build-status'];
+    const version = buildDetails['version'];
+    const buildId = buildDetails['build-id'];
+    const codebuildPageUrl = `https://${region}.console.aws.amazon.com/codesuite/codebuild/${account}/projects/${projectName}/build/${buildId}/log`;
+    const currentPhaseContext = buildDetails['current-phase-context'];
+    const messages = {
+        'IN_PROGRESS': process.env.IN_PROGRESS_MESSAGE ?? 'The build has started.',
+        'SUCCESS':  process.env.SUCCESS_MESSAGE ?? 'The build completed successfully.',
+        'FAILED': process.env.FALIED_MESSAGE ?? 'The build failed.'
+    }
+
+    // common message
+    console.log(`[${appName}] DEBUG | project-name: ${projectName}:${version}`);
+    console.log(`[${appName}] DEBUG | build-id: ${buildId}}`);
+    console.log(`[${appName}] DEBUG | build-status: ${buildStatus}`);
+    console.log(`[${appName}] DEBUG | codebuild-url: ${codebuildPageUrl}`);
+    console.log(`[${appName}] DEBUG | current-phase-context: ${currentPhaseContext}`);
+    console.log(`[${appName}] DEBUG | env: ${process.env.SAMPLE}`);
+
+    // notification
+
+
+    // response
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!!!'),
+        body: JSON.stringify({
+            'project-name': `${projectName}:${version}`,
+            'build-id': buildId,
+            'build-status': buildStatus
+        })
     };
     return response;
 };
